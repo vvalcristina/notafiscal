5d6c40f1228bc3fc9d6bb9ba444629a11ae6dd1f
Valeria Silva <silvvavaleria@gmail>
2021-03-23 19:57:04 -0300

    Removendo arquivos

--- projeto/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from projeto.core import views as v
-
-app_name ='core'
-
-urlpatterns = [
-    path('',v.index, name ='index'),
-
-]
--- projeto/empresa/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Empresa
-
-class EmpresaForm(forms.ModelForm):
-
-    class Meta:
-        model = Empresa
-        fields = '__all__'
