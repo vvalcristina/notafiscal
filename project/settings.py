8af3dcbfee7be156e3c075586c13ee07b3f7dd39
Valeria Silva <silvvavaleria@gmail>
2021-03-23 21:07:13 -0300

    Atualizaçao diretorios

---
+++ project/settings.py
@@ -0,0 +1,5 @@
+TIME_ZONE = 'America/Sao_Paulo'
+LANGUAGE_CODE = 'pt-BR'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
