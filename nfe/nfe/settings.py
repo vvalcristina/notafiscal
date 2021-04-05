3d09867abc14eb9386c0d9465cacef78d3ef8b0d
Valeria Silva <silvvavaleria@gmail>
2021-04-05 14:50:41 -0300

    Atualizando empresas

--- nfe/nfe/settings.py
+++ nfe/nfe/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'nfe.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['core'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ scripts/create_empresa.py
@@ -0,0 +1,9 @@
+from faker import Faker
+
+Faker.seed(0)
+fake = Faker('pt_br')
+
+with open('empresas.csv', 'w', encoding='utf-8') as arquivo:
+    arquivo.write('empresa,cnpj\n')
+    for item in range(20):
+        arquivo.write(f'{fake.bs()},{fake.cnpj()}\n')
